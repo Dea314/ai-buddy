36a35af6e9fefc4149725f12673a6bd407ec5391
Dea314 <fabrisdanijela314@gmail.com>
2023-09-05 15:33:25 +0200

    fixing bugs 2

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -5,6 +5,7 @@ import { stripe } from "@/lib/stripe";
 import { absoluteUrl } from "@/lib/utils";
 
 const settingsUrl = absoluteUrl("/settings");
+export const dynamic = "force-dynamic";
 
 export async function GET() {
   try {
